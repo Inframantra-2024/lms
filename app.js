a7cf3734f6d2778b12b90c941c0be49189583141
Rohit <im.software@inframantra.com>
2025-01-04 15:21:44 +0530

    video course

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ import errorMiddleware from './middleware/error.middleware.js';
 const app = express();
 
 // middleware
-app.use(express.json());  // Built-in parser for JSON bodies
-app.use(express.urlencoded({ extended: true })); // Built-in parser for URL-encoded data
+app.use(express.json({ limit: '500mb' }));  // JSON size limit (if you're sending JSON data too)
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({
